c8d34cf47247ca50d0beff199da7d610ac4e6d7b
Konweh Ikechukwu <ikebig@yahoo.com>
2017-06-27 20:19:22 +0100

    Add files via upload

---
+++ Nop.Plugin.Payments.GTPay/Validators/Validators/ConfigurationValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Nop.Plugin.Payments.GTPay.Models;
+using Nop.Services.Localization;
+using Nop.Web.Framework.Validators;
+
+namespace Nop.Plugin.Payments.GTPay.Validators
+{
+    public partial class ConfigurationValidator : BaseNopValidator<ConfigurationModel>
+    {
+        public ConfigurationValidator(ILocalizationService localizationService)
+        {
+            RuleFor(x => x.DescriptionText).NotEmpty()
+                .When(x => !x.SkipPaymentInfo)
+                .WithMessage(localizationService.GetResource(Constants.LocaleResources.GTPay_Fields_DescriptionText_Required));
+            RuleFor(x => x.MerchantId).NotEmpty()
+                .When(x => !x.UseSandbox)
+                .WithMessage(localizationService.GetResource(Constants.LocaleResources.GTPay_Fields_MerchantId_Required));
+            RuleFor(x => x.MerchantHashKey).NotEmpty()
+                .When(x => !x.UseSandbox)
+                .WithMessage(localizationService.GetResource(Constants.LocaleResources.GTPay_Fields_MerchantHashKey_Required));
+        }
+    }
+}
+
